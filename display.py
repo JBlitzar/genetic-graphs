af870fddf093283351d509d51a281a655bc94cb4
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2024-08-24 17:55:07 -0700

    save graph

--- display.py
+++ display.py
@@ -13,6 +13,23 @@ def display_graph(G):
     nx.draw_networkx_edges(G, pos, edge_color='r', arrows=True)
 
     plt.show()
+
+
+def save_graph(G,path):
+    pos = nx.kamada_kawai_layout(G)
+    
+    # Truncate labels to the first 10 characters
+    labels = {node: node[:10] for node, data in G.nodes(data=True)}
+    
+    nx.draw_networkx_nodes(G, pos, cmap=plt.get_cmap('jet'), 
+                           node_color="#ffffff", node_size=500)
+    nx.draw_networkx_labels(G, pos, labels=labels)
+    nx.draw_networkx_edges(G, pos, edge_color='r', arrows=True)
+
+    plt.savefig(path)
+    plt.close("all")
+
+
 if __name__ == "__main__":
 
     seed = 13648  # Seed random number generators for reproducibility
