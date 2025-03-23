da445ec6c441743c9f45b3b787be566e4c4c6b4b
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2025-03-23 16:10:29 -0700

    pics

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 from moduledag import ModuleDag, Module, ImageModule
-from modules import SimpleConv, ReLU
+from modules import SimpleConv, ReLU, SummationModule
 import torch
 from mutator import mutate
 import matplotlib.pyplot as plt
@@ -13,6 +13,7 @@ mg.add_module(SimpleConv("conv1"))
 mg.add_module(ReLU("relu1"))
 mg.add_module(SimpleConv("conv2"))
 mg.add_module(ReLU("relu2"))
+mg.add_module(SummationModule("summation"))
 mg.add_module(Module("OutputModule", num_inputs=1, num_outputs=1))
 
 
@@ -21,7 +22,9 @@ mg.add_connection("InputModule", "conv1")
 mg.add_connection("conv1", "relu1")
 mg.add_connection("relu1", "conv2")
 mg.add_connection("conv2", "relu2")
-mg.add_connection("relu2", "OutputModule")
+mg.add_connection("relu2", "summation")
+mg.add_connection("relu1", "summation")  # Adding a skip connection
+mg.add_connection("summation", "OutputModule")
 
 
 mg.validate_graph()
